e79b2a6a854eca004b0cd8a8fb0b9ad5878e0a65
Zhanibek <smagul.zhanibek@gmail.com>
2018-07-24 17:35:30 +0600

    Added sign in and sign out system

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
