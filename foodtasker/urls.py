937c82e6aef63074ab8eb5fba8643a07a94d953b
Zhanibek <smagul.zhanibek@gmail.com>
2018-07-25 17:43:21 +0600

    Created Restaurant model and migrated

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^restaurant/sign-in/$', auth_views.login,
         {'template_name': 'restaurant/sign_in.html'}, name='restaurant-sign-in'),
-    url(r'^restaurant/sign-out/$', auth_views.logout,
+    url(r'^restaurant/sign-out', auth_views.logout,
         {'next_page': '/'}, name='restaurant-sign-out'),
+    url(r'^restaurant/sign-up', views.restaurant_sign_up, name='restaurant_sign_up'),
     url(r'^restaurant/$', views.restaurant_home, name='restaurant-home'),
 ]
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -8,4 +8,8 @@ def home(request):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html')
+    return render(request, 'restaurant/home.html', {})
+
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign_up.html', {})
