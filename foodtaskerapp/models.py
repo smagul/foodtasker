436078c591e245e1a3c0aa4455863da26affa703
Zhanibek <smagul.zhanibek@gmail.com>
2018-08-06 19:44:51 +0600

    Added Order and OrderDetails model

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Restaurant(models.Model):
@@ -34,7 +35,7 @@ class Driver(models.Model):
 
 
 class Meal(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='meal')
+    restaurant = models.ForeignKey(Restaurant, on_delete=None)
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=500)
     image = models.ImageField(upload_to='meal_images', blank=False)
@@ -42,3 +43,39 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    COOKING = 1
+    READY = 2
+    ONTHEWAY = 3
+    DELIVERED = 4
+
+    STATUS_CHOICES = (
+        (COOKING, "Cooking"),
+        (READY, "Ready"),
+        (ONTHEWAY, "On the way"),
+        (DELIVERED, "Delivered"),
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=None)
+    restaurant = models.ForeignKey(Restaurant, on_delete=None)
+    driver = models.ForeignKey(Driver, on_delete=None)
+    address = models.CharField(max_length=200)
+    total = models.IntegerField()
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    picked_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order, on_delete=None, related_name='order_details')
+    meal = models.ForeignKey(Meal, on_delete=None)
+    quantity = models.IntegerField()
+    sub_total = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
