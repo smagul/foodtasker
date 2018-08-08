33cb56f510f08156ff0fb86121009cd9b7a1f299
Zhanibek <smagul.zhanibek@gmail.com>
2018-08-08 16:01:32 +0600

    Added getting Meal API

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
-from foodtaskerapp.models import Restaurant
-from foodtaskerapp.serializers import RestaurantSerializer
+from foodtaskerapp.models import Restaurant, Meal
+from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer
 
 
 def customer_get_restaurants(request):
@@ -14,8 +14,14 @@ def customer_get_restaurants(request):
     return JsonResponse({'restaurants': restaurants})
 
 
-def customer_get_meals(request):
-    return JsonResponse({})
+def customer_get_meals(request, restaurant_id):
+    meals = MealSerializer(
+        Meal.objects.filter(restaurant_id=restaurant_id).order_by('-id'),
+        many=True,
+        context={'request': request}
+    ).data
+
+    return JsonResponse({'meals': meals})
 
 
 def customer_add_order(request):
